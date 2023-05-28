1a213a59444c62274efcb0b0627da5ee13175ddd
Anton Rau <anton.rau@gmail.com>
2023-05-28 20:56:12 +0200

    FIX: PyInstaller multiprocessing support (see https://docs.python.org/3/library/multiprocessing.html#multiprocessing.freeze_support)

--- tse_analytics/__main__.py
+++ tse_analytics/__main__.py
@@ -1,4 +1,8 @@
+from multiprocessing import freeze_support, Process
+
 from tse_analytics.app import main
 
 if __name__ == "__main__":
-    main()
+    # See https://docs.python.org/3/library/multiprocessing.html#multiprocessing.freeze_support
+    freeze_support()
+    Process(target=main).start()
