19e7430639a91bd12112f491e87535f797a165e2
Anton Rau <anton.rau@gmail.com>
2023-11-02 15:29:58 +0100

    Help handling improvements: intermediate conversion from Markdown to HTML with embedded images (Base64).

--- tse_analytics/views/help/help_widget.py
+++ tse_analytics/views/help/help_widget.py
@@ -1,5 +1,7 @@
+from pathlib import PurePath
 from typing import Optional
 
+import markdown
 from PySide6.QtWidgets import QTextEdit, QWidget
 
 from tse_analytics.core.manager import Manager
@@ -14,10 +16,20 @@ class HelpWidget(QTextEdit, MessengerListener):
         MessengerListener.__init__(self)
         self.register_to_messenger(Manager.messenger)
 
+        self.md = markdown.Markdown(
+            extensions=["pymdownx.b64"],
+            extension_configs={
+                "pymdownx.b64": {
+                    "base_path": PurePath(__file__).parent.parent.parent / "docs"
+                }
+            }
+        )
+
         self.setReadOnly(True)
 
     def register_to_messenger(self, messenger: Messenger):
         messenger.subscribe(self, ShowHelpMessage, self._on_show_help)
 
     def _on_show_help(self, message: ShowHelpMessage):
-        self.setMarkdown(message.content)
+        html = self.md.convert(message.content)
+        self.setHtml(html)
